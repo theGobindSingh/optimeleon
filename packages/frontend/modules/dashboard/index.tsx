6db87051a2950b45d3e27ef87e9c0d9ef56519ef
Gobind Singh <thesinghgobind@gmail.com>
2025-08-06 03:24:25 +0000

    changes

--- packages/frontend/modules/dashboard/index.tsx
+++ packages/frontend/modules/dashboard/index.tsx
@@ -4,7 +4,7 @@ import { DashboardWrapper, ProjectWrapper } from "@modules/dashboard/styles";
 import { DashboardProps, Project } from "@modules/dashboard/types";
 import { AddRounded } from "@mui/icons-material";
 import Button from "@mui/material/Button";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { toast } from "react-toastify";
 
 const Dashboard = ({
@@ -12,19 +12,45 @@ const Dashboard = ({
   projects: projectsWithoutState = [],
 }: DashboardProps) => {
   const [projects, setProjects] = useState<Project[]>(projectsWithoutState);
-  const deleteProject = (projectId: string) => {
-    toast.info("Deleting project, please wait!")
+  const deleteProject = useCallback((projectId: string) => {
+    toast.info("Deleting project, please wait!");
     axiosInstance
       .delete(`/projects/${projectId}`)
       .then(() => {
         setProjects((prevProjects) => {
           return prevProjects.filter(({ id }) => id !== projectId);
         });
-        toast.success("Project Deleted!")
+        toast.success("Project Deleted!");
       })
-      .catch((error) => {
-        toast.error("Error! Project not deleted!")
+      .catch(() => {
+        toast.error("Error! Project not deleted!");
       });
+  }, []);
+
+  const projectMapper = (project: Project) => {
+    const clickHandler = () => {
+      deleteProject(project.id);
+    };
+    return (
+      <ProjectWrapper key={project.id} onClick={clickHandler}>
+        <Span>
+          <Span $weight="600">ID: </Span>
+          <Span>{project.id}</Span>
+        </Span>
+        <Span>
+          <Span $weight="600">Name: </Span>
+          <Span>{project.name}</Span>
+        </Span>
+        <Span>
+          <Span $weight="600">Target URL: </Span>
+          <Span>{project.targetUrl}</Span>
+        </Span>
+        <Span>
+          <Span $weight="600">Created at: </Span>
+          <Span>{new Date(project.createdAt).toDateString()}</Span>
+        </Span>
+      </ProjectWrapper>
+    );
   };
   return (
     <DashboardWrapper className={className} bg="var(--color-background-300)">
@@ -38,31 +64,7 @@ const Dashboard = ({
         {projects.length === 0 ? (
           <Span>No projects found</Span>
         ) : (
-          projects.map((project) => {
-            const clickHandler = () => {
-              deleteProject(project.id);
-            };
-            return (
-              <ProjectWrapper key={project.id} onClick={clickHandler}>
-                <Span>
-                  <Span $weight="600">ID: </Span>
-                  <Span>{project.id}</Span>
-                </Span>
-                <Span>
-                  <Span $weight="600">Name: </Span>
-                  <Span>{project.name}</Span>
-                </Span>
-                <Span>
-                  <Span $weight="600">Target URL: </Span>
-                  <Span>{project.targetUrl}</Span>
-                </Span>
-                <Span>
-                  <Span $weight="600">Created at: </Span>
-                  <Span>{new Date(project.createdAt).toDateString()}</Span>
-                </Span>
-              </ProjectWrapper>
-            );
-          })
+          projects.map(projectMapper)
         )}
       </main>
     </DashboardWrapper>
